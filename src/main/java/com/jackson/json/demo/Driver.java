9d50efd790cc2185dd6f0c4dc9a08ddb85896655
8e3Yn4uK <sashko.kondratenko@gmail.com>
2019-04-01 02:28:04 +0200

    nested & arrays

--- src/main/java/com/jackson/json/demo/Driver.java
+++ src/main/java/com/jackson/json/demo/Driver.java
@@ -15,10 +15,14 @@ public class Driver {
         try {
             ObjectMapper mapper = new ObjectMapper();
 
-            Student theStudent = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+            Student theStudent = mapper.readValue(new File("data/sample-full.json"), Student.class);
 
             System.out.println(theStudent.getFirstName());
             System.out.println(theStudent.getLastName());
+            Address tempAdress = theStudent.getAddress();
+            System.out.println("Street: " + tempAdress.getStreet());
+            System.out.println("City: " + tempAdress.getCity());
+            for (String tempLang : theStudent.getLanguages()) System.out.println(tempLang);
 
         } catch (Exception exc) {
             exc.printStackTrace();
--- src/main/java/com/jackson/json/demo/Student.java
+++ src/main/java/com/jackson/json/demo/Student.java
@@ -10,6 +10,8 @@ public class Student {
     private String firstName;
     private String lastName;
     private boolean active;
+    private Address address;
+    private String[] languages;
 
     public Student() {
     }
@@ -44,5 +46,21 @@ public class Student {
 
     public void setActive(boolean active) {
         this.active = active;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String[] getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String[] languages) {
+        this.languages = languages;
     }
 }
